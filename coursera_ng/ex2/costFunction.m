c58de1627ef310fecb8d0bd1105cce4e867b12c1
zhangsen <zhangsen@dr-red.com>
2017-05-25 18:19:11 +0800

    ex2 uploaded

--- coursera_ng/ex2/costFunction.m
+++ coursera_ng/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly
+% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta);
 
-sigmo = sigmoid(X * theta);
+J = (1/m) * sum(-y.*log(h)-(1-y).*log(1-h));
 
-J = 1 / m * (-y' * log(sigmo) - (1 - y') * log(1 - sigmo));
-grad = 1 / m * ((sigmo - y)' * X)';
-
+grad = (1/m) * (X'*(h-y));
 
 % =============================================================
 
--- coursera_ng/ex2/ex2_reg.m
+++ coursera_ng/ex2/ex2_reg.m
@@ -68,6 +68,26 @@ lambda = 1;
 [cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
 
 fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Expected cost (approx): 0.693\n');
+fprintf('Gradient at initial theta (zeros) - first five values only:\n');
+fprintf(' %f \n', grad(1:5));
+fprintf('Expected gradients (approx) - first five values only:\n');
+fprintf(' 0.0085\n 0.0188\n 0.0001\n 0.0503\n 0.0115\n');
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+% Compute and display cost and gradient
+% with all-ones theta and lambda = 10
+test_theta = ones(size(X,2),1);
+[cost, grad] = costFunctionReg(test_theta, X, y, 10);
+
+fprintf('\nCost at test theta (with lambda = 10): %f\n', cost);
+fprintf('Expected cost (approx): 3.16\n');
+fprintf('Gradient at test theta - first five values only:\n');
+fprintf(' %f \n', grad(1:5));
+fprintf('Expected gradients (approx) - first five values only:\n');
+fprintf(' 0.3460\n 0.1614\n 0.1948\n 0.2269\n 0.0922\n');
 
 fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
@@ -87,7 +107,7 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 0;
+lambda = 1;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
@@ -112,5 +132,5 @@ hold off;
 p = predict(theta, X);
 
 fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+fprintf('Expected accuracy (with lambda = 1): 83.1 (approx)\n');
+
